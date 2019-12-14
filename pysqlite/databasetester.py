582741e73f6551eeb941ae5d0908e12233b369db
thegoat <ruppatel11599@gmail.com>
2019-12-14 13:50:34 -0500

    first

--- pysqlite/databasetester.py
+++ pysqlite/databasetester.py
@@ -5,7 +5,7 @@ import string
 from faker import Faker
 import time
 from pydbgen import pydbgen
-from random import randrange
+from random import randrange, choice
 from datetime import timedelta, datetime
 import secrets
 import string
@@ -16,7 +16,7 @@ import string
 
 
 
-conn = sqlite3.connect('Zillow.db')
+conn = sqlite3.connect('/Users/ruppatel/DatagripProjects/ZillowDB/pysqlite/Zillow.db')
 c = conn.cursor()
 
 def create_table():
@@ -29,19 +29,19 @@ def clear_column():
 
 def insert_values():
     name = random_name()
-    for i in range(105010):
+    answer = choice(['yes','no'])
+    for i in range(101004):
         d1 = datetime.strptime('1/1/2000', '%m/%d/%Y')
         d2 = datetime.strptime('12/1/2019', '%m/%d/%Y')
 
-        #c.execute("INSERT INTO UserTest(name) VALUES (?)", ([random_name()]))
-        #c.execute("INSERT INTO Users_1(username, name, email) VALUES (?, ?, ?)", ([random_string(12), random_name(), random_email()]))
-        #c.execute("INSERT INTO Agents(specialtyid, Name, Location) VALUES (?, ?, ?)", ([random.randint(1,5), random_name(), random_city()]))
-        c.execute("INSERT INTO AgentsReviews(comments, userid, agentid) VALUES (?, ?, ?)", ([random_comment(), random.randint(1, 100000), random.randint(1,100000)]))
-        #c.execute("INSERT INTO Houses(stateid, price, ForTypeid, bedrooms, SquareFeet, HomeTypeid, dateAdded) VALUES (?, ?, ?, ?, ?, ?, ?)", ([random.randint(1,50), random.randint(25000,100000000), random.randint(1,3), random.randint(1,10), random.randint(1000, 100000), random.randint(1,5), random_date(d1,d2)]))
-        #c.execute("INSERT INTO Users(username, name, email, datejoined, password) VALUES (?, ?, ?,?,?)", ([random_string(12), random_name(), random_email(), random_date(d1,d2), random_password()]))
-        #c.execute("INSERT INTO Houses_1(userid) VALUES (?)", ([random.randint(114, 213)]))
-        #c.execute("Insert into Listings(Houseid, agentid) VALUES (?,?)", ([random.randint(1, 100000), random.randint(1,100000)]))
-        #c.execute("INSERT INTO SavedHouses(userid, houseid)  VALUES (?, ?)", ([random.randint(1,10402), (random.randint(1,10402))]))
+
+        c.execute("INSERT INTO Agents(specialtyid, Name, Location, stateid) VALUES (?, ?, ?,?)", ([random.randint(1,5), random_name(), random_city(), random.randint(1,50)]))
+        c.execute("INSERT INTO AgentReviews(comments, userid, agentid) VALUES (?, ?, ?)", ([random_comment(), random.randint(1, 101004), random.randint(1,100000)]))
+        c.execute("INSERT INTO House(price, ForTypeid, bedrooms, SquareFeet, HomeTypeid, dateAdded, Address) VALUES (?, ?, ?, ?, ?, ?,?)", ([random.randint(25000,100000000), random.randint(1,3), random.randint(1,10), random.randint(1000, 100000), random.randint(1,5), random_date(d1,d2), random_address() ]))
+        c.execute("INSERT INTO Users(username, name, email, datejoined, password) VALUES (?, ?, ?,?,?)", ([random_string(12), random_name(), random_email(), random_date(d1,d2), random_password()]))
+        c.execute("Insert into Listings(Houseid, agentid) VALUES (?,?)", ([random.randint(1, 100000), random.randint(1,100000)]))
+        c.execute("INSERT INTO SavedHouses(userid, houseid)  VALUES (?, ?)", ([random.randint(1,10402), (random.randint(1,10402))]))
+        c.execute("INSERT INTO Mortgages(stateid, TypeRateid, LoanTypeid,LoanAmount, DownPayment, CreditScore, Veteran, userid, rateAmount) VALUES (?, ?, ?, ?, ?, ?, ?,?,?)", ([random.randint(1,50), random.randint(1,2), random.randint(1,5), random.randint(1,1000000), random.randint(1000, 50000), random.randint(300,850), answer, random.randint(1,115512), random.uniform(1.0,12.0)]))
 
 
     conn.commit()
@@ -72,7 +72,9 @@ def random_date(start, end):
     random_second = randrange(int_delta)
     return start + timedelta(seconds=random_second)
 
-
+def random_address():
+    fake=Faker()
+    return fake.address()
 
 def random_city():
     my_db = pydbgen.pydb()
